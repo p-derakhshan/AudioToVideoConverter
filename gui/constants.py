bda1955e723a2d460a52987ecf651ee3c3538355
pder <parya_derakhshan@yahoo.com>
2021-05-03 20:04:03 +0430

    create class for constant variables in gui

---
+++ gui/constants.py
@@ -0,0 +1,15 @@
+class Constants():
+    SC_WIDTH, SC_HEIGHT = 600, 450
+    SC_X, SC_Y = 250, 150
+    LBL_WIDTH, LBL_HEIGHT = 550, 10
+    BTN_WIDTH, BTN_HEIGHT = 80, 25
+    # FONT = ??
+
+    def color_blue_light(self):
+        return  "color: rgb(197, 255, 254);\n"
+    
+    def color_blue_dark(self):
+        return "color: rgb(94, 130, 180);\n"
+
+    def color_white(self):
+        return "color: rgb(0, 0, 0);\n"
