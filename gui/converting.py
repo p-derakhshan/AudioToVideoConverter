9b6d3d3d256139e976cc4aa76d0e641190adaee0
Mahshid <52647056+mahshidkhatiri@users.noreply.github.com>
2021-05-11 16:53:50 +0430

    progress bar

--- gui/converting.py
+++ gui/converting.py
@@ -1,7 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from constants import Constants
-import os
-
+import os, time
 C= Constants()
 
 class Ui_MainWindow(QtWidgets.QMainWindow):
@@ -17,7 +16,6 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
         self.setupUi()
-
     def setupUi(self):
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(30, 70, 91, 31))
@@ -30,38 +28,40 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.progressBar.setStyleSheet("QProgressBar::chunk { background-"+C.color_blue_dark()+
 "}\n"
 "")
-        self.progressBar.setProperty("value", 24)
+        self.progressBar.setProperty("value", 0)
         self.progressBar.setAlignment(QtCore.Qt.AlignCenter)
         self.progressBar.setObjectName("progressBar")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(440, 400, 75, 23))
+        self.pushButton.setGeometry(QtCore.QRect(520, 400, 75, 23))
         self.pushButton.setStyleSheet("background-"+C.color_blue_dark()+C.color_blue_light())
-        self.pushButton.setObjectName("pushButton")
+        self.pushButton.setObjectName("pushButton_2")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(520, 400, 75, 23))
+        self.pushButton_2.setGeometry(QtCore.QRect(210, 200, 151, 31))
         self.pushButton_2.setStyleSheet("background-"+C.color_blue_dark()+C.color_blue_light())
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(210, 200, 151, 31))
-        self.pushButton_3.setStyleSheet("background-"+C.color_blue_dark()+C.color_blue_light())
-        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_2.setObjectName("pushButton_3")
+        self.pushButton_2.setHidden(True)
         self.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(self)
         self.statusbar.setObjectName("statusbar")
         self.setStatusBar(self.statusbar)
-
         self.retranslateUi()
         QtCore.QMetaObject.connectSlotsByName(self)
+        
 
     def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
         self.setWindowTitle(_translate("AudioToVideo", "AudioToVideo"))
         self.label.setText(_translate("MainWindow", "converting"))
+        self.pushButton.setText(_translate("MainWindow", "Cancel"))
+        self.pushButton_2.setText(_translate("MainWindow", "Open file directory"))
+    def progressBar_method(self):
+        _translate = QtCore.QCoreApplication.translate
+        for i in range(101):
+            time.sleep(0.01)
+            self.progressBar.setValue(i)
+        self.pushButton_2.setHidden(False)
         self.pushButton.setText(_translate("MainWindow", "Again"))
-        self.pushButton_2.setText(_translate("MainWindow", "Cancel"))
-        self.pushButton_3.setText(_translate("MainWindow", "Open file directory"))
-
-
+    
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
