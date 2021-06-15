c23965a8c9257c1ae830975cad2b75ee1826a505
Mahshid <52647056+mahshidkhatiri@users.noreply.github.com>
2021-06-15 22:26:19 +0430

    UI

--- gui/ui.py
+++ gui/ui.py
@@ -1,12 +1,73 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from constants import Constants
-import sys, converting, os
-
+import sys ,  os,time,webbrowser
 C= Constants()
 
-class Ui_MainWindow(QtWidgets.QMainWindow):
+class Ui_Welcome_MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Ui_MainWindow, self).__init__()
+        super(Ui_Welcome_MainWindow, self).__init__()
+        self.setObjectName("MainWindow")
+        self.setGeometry(C.SC_X,C.SC_Y,C.SC_WIDTH,C.SC_HEIGHT)
+        self.setAutoFillBackground(False)
+        self.setStyleSheet("background-"+C.color_blue_light())
+        self.file_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__))+ os.sep + os.pardir)
+        icon_path = "sprites/icon.png"
+        file_path = os.path.join(self.file_dir, icon_path)
+        self.setWindowIcon(QtGui.QIcon(file_path))
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.ui_dir = Ui_Directories_MainWindow()
+        self.setupUi()
+        
+
+    def setupUi(self):
+        '''image label'''
+        self.lbl_img = QtWidgets.QLabel(self.centralwidget)
+        self.lbl_img.setGeometry(QtCore.QRect(20, 70, 281, 401))
+        self.lbl_img.setText("") 
+        rel_path = "sprites/welcome_transparent.png"
+        abs_file_path = os.path.join(self.file_dir, rel_path)
+        pixmap = QtGui.QPixmap(abs_file_path)
+        self.lbl_img.setPixmap(pixmap)
+        self.lbl_img.setObjectName("lbl_img_welcome")
+        self.lbl_img.resize(pixmap.width(),pixmap.height())
+        '''start button'''
+        self.btn_start = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_start.setGeometry(QtCore.QRect(370, 150, 151, 141))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.btn_start.setFont(font)
+        self.btn_start.setStyleSheet("background-"+C.color_blue_dark()+C.color_blue_light())
+        self.btn_start.setObjectName("btn_start")
+        self.btn_start.clicked.connect(self.click_start)
+        '''welcome label'''
+        self.lbl_welcome = QtWidgets.QLabel(self.centralwidget)
+        self.lbl_welcome.setGeometry(QtCore.QRect(25, 35, 281, 25))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.lbl_welcome.setFont(font)
+        self.lbl_welcome.setAlignment(QtCore.Qt.AlignCenter)
+        self.lbl_welcome.setObjectName("lbl_welcome")
+        self.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName("statusbar")
+        self.setStatusBar(self.statusbar)
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("AudioToVideo", "AudioToVideo"))
+        self.btn_start.setText(_translate("MainWindow", "START"))
+        self.lbl_welcome.setText(_translate("MainWindow", "Welcome!"))
+
+    def click_start(self):
+        self.ui_dir.show()
+        self.close()
+class Ui_Directories_MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui_Directories_MainWindow, self).__init__()
         self.setObjectName("MainWindow")
         self.setGeometry(C.SC_X,C.SC_Y,C.SC_WIDTH,C.SC_HEIGHT)
         self.setStyleSheet("background-"+C.color_blue_light())
@@ -177,9 +238,98 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.output_dir = self.txt_output_dir.text()
         self.script_dir = self.txt_script_dir.text() 
         if self.cheak_format():
-            self.ui_convert = converting.Ui_MainWindow(self.audio_dir,self.output_dir, self.script_dir)
-            self.ui_convert.show()
+            self.ui_convert = Ui_Converting_MainWindow(self.audio_dir,self.output_dir, self.script_dir)
             self.close()
+            self.ui_convert.show()
+class Ui_Converting_MainWindow(QtWidgets.QMainWindow):
+    def __init__(self,audio_dir,output_dir,script_dir):
+        super(Ui_Converting_MainWindow, self).__init__()
+        self.setObjectName("MainWindow")
+        self.setGeometry(C.SC_X,C.SC_Y,C.SC_WIDTH,C.SC_HEIGHT)
+        self.setStyleSheet("background-"+C.color_blue_light())
+        self.file_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__))+ os.sep + os.pardir)
+        icon_path = "sprites/icon.png"
+        file_path = os.path.join(self.file_dir, icon_path)
+        self.setWindowIcon(QtGui.QIcon(file_path))
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.audio_dir=audio_dir
+        self.output_dir=output_dir
+        self.script_dir=script_dir
+        self.ui_wel = Ui_Welcome_MainWindow()
+        self.done=False
+        self.setupUi()
+    def setupUi(self):
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(30, 70, 91, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setGeometry(QtCore.QRect(30, 110, 531, 31))
+        self.progressBar.setStyleSheet("QProgressBar::chunk { background-"+C.color_blue_dark()+
+"}\n"
+"")
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setAlignment(QtCore.Qt.AlignCenter)
+        self.progressBar.setObjectName("progressBar")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(520, 400, 75, 23))
+        self.pushButton.setStyleSheet("background-"+C.color_blue_dark()+C.color_blue_light())
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.click)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(210, 200, 151, 31))
+        self.pushButton_2.setStyleSheet("background-"+C.color_blue_dark()+C.color_blue_light())
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setHidden(True)
+        self.pushButton_2.clicked.connect(self.open_folder)
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(440, 400, 75, 23))
+        self.pushButton_3.setStyleSheet("background-"+C.color_blue_dark()+C.color_blue_light())
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.clicked.connect(self.progressBar_method)
+        self.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName("statusbar")
+        self.setStatusBar(self.statusbar)
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
+        
 
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("AudioToVideo", "AudioToVideo"))
+        self.label.setText(_translate("MainWindow", " "))
+        self.pushButton.setText(_translate("MainWindow", "Cancel"))
+        self.pushButton_2.setText(_translate("MainWindow", "Open file directory"))
+        self.pushButton_3.setText(_translate("MainWindow", "START"))
+    def progressBar_method(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.label.setText(_translate("MainWindow", "converting..."))
+        self.pushButton_3.setHidden(True)
+        for i in range(101):
+            time.sleep(0.01)
+            self.progressBar.setValue(i)
+        self.done=True
+        self.pushButton_2.setHidden(False)
+        self.label.setText(_translate("MainWindow", "Done"))
+        self.pushButton.setText(_translate("MainWindow", "Again"))
+    def open_folder(self):
+        webbrowser.open(self.output_dir)
+    def click(self):
+        if self.done:
+            self.ui_wel.show()
+            self.close()
+        else:
+            self.close()
+       
 
-
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    ui = Ui_Welcome_MainWindow()
+    ui.show()
+    sys.exit(app.exec_())
+if __name__ == "__main__":
+    main()
