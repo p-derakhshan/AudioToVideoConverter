75e788d5014d4ee9c9eba1213f0d777f64ffd163
p der <parya_derakhshan@yahoo.com>
2021-06-23 17:31:18 +0430

    fix get_wav_file dir variables

--- source/tools.py
+++ source/tools.py
@@ -3,7 +3,6 @@ from pydub import AudioSegment
 
 class Tools:
     def annotation_remover(self, script_file, output_dir):
-        
         with open(script_file, "r") as input:
             with open(output_dir, "w") as output:
                 for line in input:
@@ -14,16 +13,12 @@ class Tools:
                         line = line.replace('&', 'and')
                         output.write(line) 
 
-    def get_wav_file(self, audio_dir, files_dir):
-        input_dir, audio = os.path.split(audio_dir)
-        file_name = '/{}.wav'.format(os.path.splitext(self.file_name)[0])
-        file = self.file_path+file_name
-        if not self.file_name.endswith('.wav'):
-            new_file= self.files+file_name
-            AudioSegment.from_file(file).export(new_file,format='wav')
-            file = new_file
-        return file
-        #if filename.endswith('.mp3') or filename.endswith('.flac'):
+    def get_wav_file(self, audio, name, files_dir):
+        if not audio.endswith('.wav'):
+            new_file= files_dir+name+'.wav'
+            AudioSegment.from_file(audio).export(new_file,format='wav')
+            audio= new_file
+        return audio
 
     def tuning(self,file_wav):
         wav_file = AudioSegment.from_file(file=file_wav)
